9b482795e8b15dd40e265b693b88a94918b21643
Khulud Zumurrud <unsociable.catgirl@gmail.com>
2026-09-05 21:48:08 -0600

    what am I even doing

---
+++ _app/immutable/chunks/paths.1255517a.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1skw051)==null?void 0:s.base)??"/asbestos-is-bestos";var e;const a=((e=globalThis.__sveltekit_1skw051)==null?void 0:e.assets)??t;export{a,t as b};
